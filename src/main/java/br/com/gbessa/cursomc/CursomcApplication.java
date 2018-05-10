b0b9715a8e9ad82a1ef244f8297f8bd2a2240b49
gbessa <gbvirtual@gmail.com>
2018-05-10 08:29:34 -0300

    Populating DB on init

--- src/main/java/br/com/gbessa/cursomc/CursomcApplication.java
+++ src/main/java/br/com/gbessa/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package br.com.gbessa.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.gbessa.cursomc.domain.Categoria;
+import br.com.gbessa.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
     public static void main(String[] args) {
 	SpringApplication.run(CursomcApplication.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "TIC");
+		Categoria cat2 = new Categoria(null, "Administrativo");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }
