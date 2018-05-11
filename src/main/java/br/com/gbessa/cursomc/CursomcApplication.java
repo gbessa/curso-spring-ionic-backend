38c5d6d5135b3c9e55a6e6be4a9ee5d795d4aded
Gustavo Bessa <gbvirtual@gmail.com>
2018-05-11 13:45:47 -0300

    Produto e associação de Produto e Categoria

--- src/main/java/br/com/gbessa/cursomc/CursomcApplication.java
+++ src/main/java/br/com/gbessa/cursomc/CursomcApplication.java
@@ -8,24 +8,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.gbessa.cursomc.domain.Categoria;
+import br.com.gbessa.cursomc.domain.Produto;
 import br.com.gbessa.cursomc.repositories.CategoriaRepository;
+import br.com.gbessa.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
 	SpringApplication.run(CursomcApplication.class, args);
     }
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Categoria cat1 = new Categoria(null, "TIC");
-		Categoria cat2 = new Categoria(null, "Administrativo");
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
-	}
+    @Override
+    public void run(String... args) throws Exception {
+
+	Categoria cat1 = new Categoria(null, "Informática");
+	Categoria cat2 = new Categoria(null, "Escritório");
+
+	Produto p1 = new Produto(null, "Computador", 2000.0);
+	Produto p2 = new Produto(null, "Impressora", 800.0);
+	Produto p3 = new Produto(null, "Mouse", 80.0);
+
+	cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+	cat2.getProdutos().addAll(Arrays.asList(p2));
+
+	p1.getCategorias().addAll(Arrays.asList(cat1));
+	p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+	p3.getCategorias().addAll(Arrays.asList(cat1));
+
+	categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    }
 }
--- src/main/java/br/com/gbessa/cursomc/domain/Produto.java
+++ src/main/java/br/com/gbessa/cursomc/domain/Produto.java
@@ -8,61 +8,69 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Produto implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
-    public Integer getId() {
-		return id;
-	}
+    private Double preco;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-
-	public List<Categoria> getCategorias() {
-		return categorias;
-	}
-
-	public void setCategorias(List<Categoria> categorias) {
-		this.categorias = categorias;
-	}
-
-	private Double preco;
-
+    @ManyToMany
+    @JoinTable(name="PRODUTO_CATEGORIA",
+    	joinColumns=@JoinColumn(name="produto_id"),
+    	inverseJoinColumns=@JoinColumn(name="categoria_id")
+    )
     private List<Categoria> categorias = new ArrayList<>();
     
-    public Produto() {
-    	
+    public Integer getId() {
+	return id;
     }
 
-	public Produto(Integer id, String nome, Double preco) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.preco = preco;
-	}
-    
-    
+    public void setId(Integer id) {
+	this.id = id;
+    }
+
+    public String getNome() {
+	return nome;
+    }
+
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
+
+    public Double getPreco() {
+	return preco;
+    }
+
+    public void setPreco(Double preco) {
+	this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+	return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+	this.categorias = categorias;
+    }
+
+
+    public Produto() {
+
+    }
+
+    public Produto(Integer id, String nome, Double preco) {
+	super();
+	this.id = id;
+	this.nome = nome;
+	this.preco = preco;
+    }
+
 }
