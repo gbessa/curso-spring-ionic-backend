aa6f79aa95a64c54a6500bcc4b770860db9db25f
Gustavo Bessa <gbvirtual@gmail.com>
2018-05-16 15:11:27 -0300

    Listagem de categorias Paginadas (Page)

--- src/main/java/br/com/gbessa/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/gbessa/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -30,38 +32,50 @@ public class CategoriaResource {
     @GetMapping()
     public ResponseEntity<List<CategoriaDTO>> findAll() {
 	List<Categoria> list = service.findAll();
-	List<CategoriaDTO> listDto = list.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+	List<CategoriaDTO> listDto = list.stream().map(categoria -> new CategoriaDTO(categoria))
+		.collect(Collectors.toList());
 	return ResponseEntity.ok().body(listDto);
     }
-    
-    //@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-    @GetMapping(value="/{id}")
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+	    @RequestParam(name = "page", defaultValue = "0") Integer page,
+	    @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+	    @RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
+	    @RequestParam(name = "direction", defaultValue = "ASC") String direction) {
+	Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+	Page<CategoriaDTO> listDto = list.map(categoria -> new CategoriaDTO(categoria));
+	return ResponseEntity.ok().body(listDto);
+    }
+
+    // @RequestMapping(value = "/{id}", method=RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 	Categoria obj = service.find(id);
 	return ResponseEntity.ok().body(obj);
     }
-    
-    //@RequestMapping(method=RequestMethod.POST)
+
+    // @RequestMapping(method=RequestMethod.POST)
     @PostMapping
-    public ResponseEntity<Void> insert(@RequestBody Categoria obj) { //Essa anotação é para converter o JSON em objeto java
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) { // Essa anotação é para converter o JSON em objeto
+								     // java
 	obj = service.insert(obj);
 	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 	return ResponseEntity.created(uri).build();
     }
-    
-    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
-	obj.setId(id); //só para garantir rs
+	obj.setId(id); // só para garantir rs
 	obj = service.update(obj);
 	return ResponseEntity.noContent().build();
     }
-    
-    //@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
-    @DeleteMapping(value="/{id}")
+
+    // @RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
 	service.delete(id);
 	return ResponseEntity.noContent().build();
     }
-    
-   
+
 }
