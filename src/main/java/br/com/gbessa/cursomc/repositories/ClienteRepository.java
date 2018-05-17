8935df4b7c33d8fa533eda1b7fc5589fb636ced8
Gustavo Bessa <gbvirtual@gmail.com>
2018-05-17 17:40:18 -0300

    Validação customizada de Email já existente

--- src/main/java/br/com/gbessa/cursomc/repositories/ClienteRepository.java
+++ src/main/java/br/com/gbessa/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,15 @@ package br.com.gbessa.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.gbessa.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+    //Como usamos o nome do campo, o spring já entende e para a busca para nós    
+    @Transactional(readOnly=true)
+    Cliente findByEmail(String email); 
+    
 }
--- src/main/java/br/com/gbessa/cursomc/services/validation/ClienteInsertValidador.java
+++ src/main/java/br/com/gbessa/cursomc/services/validation/ClienteInsertValidador.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.gbessa.cursomc.domain.Cliente;
 import br.com.gbessa.cursomc.dto.ClienteNewDTO;
 import br.com.gbessa.cursomc.enums.TipoCliente;
+import br.com.gbessa.cursomc.repositories.ClienteRepository;
 import br.com.gbessa.cursomc.resources.exceptions.FieldMessage;
 import br.com.gbessa.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidador implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+    @Autowired
+    private ClienteRepository repo;
+    
     @Override
     public void initialize(ClienteInsert ann) {
 	
@@ -28,6 +35,11 @@ public class ClienteInsertValidador implements ConstraintValidator<ClienteInsert
 	}
 	if (objDto.getTipo() == TipoCliente.PESSOAJURIDICA.getCod() && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 	    list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+	}
+	
+	Cliente aux = repo.findByEmail(objDto.getEmail());
+	if (aux != null) {
+	    list.add(new FieldMessage("email", "E-mail já existente"));
 	}
 	
 	for (FieldMessage  e : list) {
