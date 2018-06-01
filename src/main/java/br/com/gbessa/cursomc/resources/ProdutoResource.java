3927aa29519590b507449ff1aef862323b0043b4
Gustavo Bessa <gbvirtual@gmail.com>
2018-06-01 08:19:32 -0300

    Inserindo Endpoint produtos/id

--- src/main/java/br/com/gbessa/cursomc/resources/ProdutoResource.java
+++ src/main/java/br/com/gbessa/cursomc/resources/ProdutoResource.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +24,14 @@ public class ProdutoResource {
     @Autowired
     private ProdutoService service;
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Produto> find(@PathVariable Integer id) {
+	Produto obj = service.find(id);
+	return ResponseEntity.ok().body(obj);
+    }
+
     @GetMapping()
-    public ResponseEntity<Page<ProdutoDTO>> findPage(
-	    @RequestParam(name = "nome", defaultValue = "") String nome,
+    public ResponseEntity<Page<ProdutoDTO>> findPage(@RequestParam(name = "nome", defaultValue = "") String nome,
 	    @RequestParam(name = "categorias", defaultValue = "") String categorias,
 	    @RequestParam(name = "page", defaultValue = "0") Integer page,
 	    @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
